4e7aad835a7753cfa4d3a85fd566298f04e99d91
NielsKornerup <nielsskornerup@gmail.com>
2016-09-30 01:33:54 -0500

    added primitive xkcd command

--- bot.js
+++ bot.js
@@ -22,10 +22,12 @@ function respond() {
 }
 
 function postMessage(text) {
+  var isImage = false;
+  var image = "";
   var botResponse, options, body, botReq;
   console.log("Current text is: " + text);
   if(/^help/.test(text)){
-     botResponse = "current valid commands are: \n test - the bot passes the turing test. \n echo [text] - the turing bot says [text] \n halts [program p] [input i] - determines if p will halt with input i \n recurse [text] - prints a recursed version of [text]. \n random - gives you an integer between 0 and 99. \n quote - gives one of a collection of quotes \n help - displays this information.";
+     botResponse = "current valid commands are: \n test - the bot passes the turing test. \n echo [text] - the turing bot says [text] \n halts [program p] [input i] - determines if p will halt with input i \n recurse [text] - prints a recursed version of [text]. \n random - gives you an integer between 0 and 99. \n quote - gives one of a collection of quotes \n xkcd [comic_name] - finds the xkcd with the given name. \n help - displays this information.";
   }
   else if(/^test$/.test(text)){
      botResponse = "I am a human.";
@@ -36,8 +38,8 @@ function postMessage(text) {
   else if(/^recurse .*/.test(text)){
     var initial = text.substring(8);
     botResponse = "";
-    for(var i = 0; i <= initial.length; i++){
-      botResponse += initial.substring(0,i);
+    for(var i = 0; i < initial.length; i++){
+      botResponse += initial.substring(i, initial.length);
     }
   }
   else if(/^halts .*/.test(text)){
@@ -50,6 +52,16 @@ function postMessage(text) {
   }
   else if(/^random$/.test(text)){
     botResponse = String((Math.floor(Math.random() * 100)));
+  }
+  else if(/^xkcd .+/.test(text)){
+    var data = text.split;
+    if(data.length == 2){
+      isImage = true;
+      image = "http://imgs.xkcd.com/comics/"+data[1]+".png";
+    }
+    else{
+      botResponse = "Invalid xkcd format. Proper format is comic name in lowercase with _ for spaces."
+    }
   }
   else if(/^quote$/.test(text)){
     var x = Math.random();
@@ -73,10 +85,22 @@ function postMessage(text) {
     method: 'POST'
   };
 
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
+  if(!isImage){  
+    body = {
+      "bot_id" : botID,
+      "text" : botResponse
+    };
+  }
+  else{
+    body = {
+      "bot_id" : botID,
+      "text" : "",
+      "attachments" : [{
+        "type"  : "image",
+        "url"   : image
+      }]
+    }
+  }
 
   console.log('sending ' + botResponse + ' to ' + botID);
 
