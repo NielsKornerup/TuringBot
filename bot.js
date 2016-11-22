2da2e45c1bbe6bde537d1aa667e7d5e192261d01
NielsKornerup <nielsskornerup@gmail.com>
2016-11-22 00:09:56 -0600

    changed keyword for 8ball

--- bot.js
+++ bot.js
@@ -92,7 +92,7 @@ function postMessage(name, text) {
   var botResponse, options, body, botReq;
   console.log("Current text is: " + text);
   if(/^help/.test(text)){
-     botResponse = "current valid commands are:\n test - the bot passes the turing test.\n echo [text] - the turing bot says [text]\n halts [program p] [input i] - determines if p will halt with input i\n recurse [text] - prints a recursed version of [text].\n random - gives you an integer between 0 and 99.\n EightBall - generates a random 8-ball response.\n quote - gives one of a collection of quotes.\n quote add [text] - adds a quote to the list.\n xkcd [comic_name] - finds the xkcd with the given name.\n status [text] - sets the bots status to [text] if no text is provided, gives the current status.\n lmgtfy [text] - googles the desired text.\n feels - displays how Alan feels right now.\n go away - tell the TuringMachine to go away.\n latex - returns image containing LaTeX render of your input.\n help - displays this information.";
+     botResponse = "current valid commands are:\n test - the bot passes the turing test.\n echo [text] - the turing bot says [text]\n halts [program p] [input i] - determines if p will halt with input i\n recurse [text] - prints a recursed version of [text].\n random - gives you an integer between 0 and 99.\n 8ball - generates a random 8-ball response.\n quote - gives one of a collection of quotes.\n quote add [text] - adds a quote to the list.\n xkcd [comic_name] - finds the xkcd with the given name.\n status [text] - sets the bots status to [text] if no text is provided, gives the current status.\n lmgtfy [text] - googles the desired text.\n feels - displays how Alan feels right now.\n go away - tell the TuringMachine to go away.\n latex - returns image containing LaTeX render of your input.\n help - displays this information.";
   }
   else if(/^test$/.test(text)){
      botResponse = "I am a human.";
@@ -126,7 +126,7 @@ function postMessage(name, text) {
       botResponse = "good man";
     }
   }
-  else if(/^EightBall.*/.test(text)){
+  else if(/^8ball.*/.test(text)){
 	var EightBallResponses = [
 		"Most definitely yes.",
 		"For sure.",
